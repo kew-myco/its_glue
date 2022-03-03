ef90f96838589db79c9a2d6f0f747a98eb9c8acf
blex <ablex7@gmail.com>
2022-03-03 13:39:25 +0000

    better create_env

--- scripts/CREATE_ENV.sh
+++ scripts/CREATE_ENV.sh
@@ -13,9 +13,6 @@ mkdir ./results
 wget -O ./tracy/tracy https://github.com/gear-genomics/tracy/releases/download/v0.7.1/tracy_v0.7.1_linux_x86_64bit
 chmod +x ./tracy/tracy
 
-conda create -p ./seq_conda
-conda activate ./seq_conda
-conda install -c bioconda itsx vsearch
-conda install -c conda-forge biopython
+conda create -p ./seq_conda -c conda-forge -c bioconda biopython itsx vsearch
 
 
