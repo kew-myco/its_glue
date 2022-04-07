2b093ada14913075a62703f610ddb8ae77cfc0db
blex <ablex7@gmail.com>
2022-04-07 10:11:00 +0100

    days work

--- scripts/modules/xtract_notmatched_sintax.py
+++ scripts/modules/xtract_notmatched_sintax.py
@@ -9,9 +9,7 @@ Created on Wed Mar  9 15:03:12 2022
 from argparse import ArgumentParser
 import re
 import pandas as pd
-import csv
 from Bio import SeqIO
-from Bio import Seq
 
 parser = ArgumentParser()
 parser.add_argument("sintax_class", help="path to input sintax classifications")
@@ -20,30 +18,32 @@ parser.add_argument("op", help="output path")
 
 args = parser.parse_args()
 
-with open(args.sintax_class) as sc, open(args.input_fasta) as f:
+with open(args.sintax_class) as sc, open(args.input_fasta) as sf:
     clsif = pd.read_csv(sc, sep = '\t', header=None, names=list('abcde'))
-    sq = f.readlines()
+    sq = [x for x in SeqIO.parse(sf, 'fasta-2line')]
     
 clsif_l = clsif.values.tolist()
 
 clif_s = [[str(x) for x in row] for row in clsif_l]
 
-no_sh = [[row[0], row[3]] for row in clif_s if not re.search('SH[0-9]', row[3])] # samples which don't match to SH
+no_sh = [row[0] for row in clif_s if not re.search('SH[0-9]', row[3])] # samples which don't match to SH
 
-
-# SeqIO.read(f, format)
+no_sh_sq = [s for s in sq if s.id in no_sh]
 
 # write output
 
 with open(args.op, 'w') as o :
-    [o.write(x + '\n') for x in no_sh]
+    SeqIO.write(no_sh_sq, o, 'fasta-2line')
     
-    
-    
-    
-### test code
 
-# with open('/home/blex/Documents/Kew/fungi_research/manuel/seqs.07_03_22/pipe_out/OTU/sintax_class.tsv') as sc:
+    
+    
+# test code
+
+# with open('/home/blex/Documents/Kew/fungi_research/george/classifier_output/6_512.1/its_sintax_class.tsv') as sc:
 #     clsif = pd.read_csv(sc, sep = '\t', header=None, names=list('abcde'))
 
-# with open('')
+# with open('/home/blex/Documents/Kew/fungi_research/george/trace_processing/6_512.1/its/cat_its.fa') as sf:
+#     sq = [x for x in SeqIO.parse(sf, 'fasta-2line')]
+
+# SeqIO.write(no_sh_sq, '/home/blex/Documents/Kew/fungi_research/george/classifier_output/6_512.1/no_match.fasta', 'fasta-2line')
