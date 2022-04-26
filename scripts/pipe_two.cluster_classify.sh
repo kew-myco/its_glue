b27117cd7cfc951d153363c382c18e763e2300bc
blex <ablex7@gmail.com>
2022-04-26 16:56:47 +0100

    variable sintax and clustering cutoffs

--- scripts/pipe_two.cluster_classify.sh
+++ scripts/pipe_two.cluster_classify.sh
@@ -16,13 +16,14 @@
 
 set -e
 
-while getopts d:f:o:x flag
+while getopts d:f:o:c:s: flag
 do
     case "${flag}" in
+        c) cid=${OPTARG};;
         d) db=${OPTARG};;
         f) fasta=${OPTARG};;
         o) out_dir=${OPTARG};;
-        x) overwrite="y";;
+        s) sco=${OPTARG};;
 
         *) echo "ERROR: invalid flag! Have you read the README?" 
            exit 1
@@ -38,6 +39,16 @@ fi
 
 if [[ "$out_dir" == "" ]] ; then
     out_dir='.'
+fi
+
+if [ "$cid" == "" ] ; then
+    echo "warning: no clustering id provided (-c), defaulting to 0.97" >&2
+    cid=0.97
+fi
+
+if [ "$sco" == "" ] ; then
+    echo "warning: no sintax bootstrap cutoff provided (-s), defaulting to 0.6" >&2
+    sco=0.6
 fi
 
 #activate conda
@@ -56,12 +67,12 @@ fi
 # --maxaccepts: number of decent hits to look for before making a decision (default 1!)
 vsearch --cluster_size "${fasta}" \
 --otutabout "${out_dir}"/cluster_membership.tsv \
---id 0.97 \
+--id "${cid}" \
 --sizeorder --clusterout_sort
 
 vsearch --sintax "${fasta}" \
 --db "${db}" \
---sintax_cutoff 0.6 \
+--sintax_cutoff "${sco}" \
 --tabbedout "${out_dir}"/sintax_classifications.tsv
 
 python3 ./scripts/modules/cluster_classify_organise_output.py "${out_dir}"/sintax_classifications.tsv "${out_dir}"/cluster_membership.tsv "${out_dir}"
